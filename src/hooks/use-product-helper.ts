fd53cca3e7017125e5f79b09d587a7b50e3c6ea9
Julian Benegas <julianbenegas99@gmail.com>
2022-05-11 13:28:48 -0300

    cleanup

--- src/hooks/use-product-helper.ts
+++ src/hooks/use-product-helper.ts
@@ -5,9 +5,6 @@ import { useStorefront } from '../lib/shopify/storefront/provider'
 
 export const useProductHelper = (product: ProductFragment) => {
   const { onAddLineItem, cartToggleState } = useStorefront()
-  const [addToBagState, setAddToBagState] = useState<
-    'idle' | 'loading' | 'success' | 'error'
-  >('idle')
 
   const [selectedOptions, setSelectedOptions] = useState<
     Record<string, string | undefined>
@@ -15,7 +12,7 @@ export const useProductHelper = (product: ProductFragment) => {
     const opts: Record<string, string | undefined> = {}
 
     product.options.forEach((o) => {
-      opts[o.name] = undefined
+      if (o.name.toLowerCase() !== 'title') opts[o.name] = undefined
     })
 
     return opts
@@ -30,9 +27,8 @@ export const useProductHelper = (product: ProductFragment) => {
   const selectedVariant = useMemo(() => {
     if (product.variants.edges.length === 1) return product.variants.edges[0]
 
+    const _selectedOptions = Object.entries(selectedOptions)
     const selectedVariant = product.variants.edges.find((variant) => {
-      const _selectedOptions = Object.entries(selectedOptions)
-
       return _selectedOptions.every(([name, value]) =>
         variant.node.selectedOptions.find(
           (option) => option.name === name && option.value === value
@@ -43,80 +39,112 @@ export const useProductHelper = (product: ProductFragment) => {
     return selectedVariant
   }, [selectedOptions, product.variants])
 
-  const cleanOptions = useMemo(
+  const optionsToSelect = useMemo(
     () =>
       product.options
+        .filter((o) => o.name.toLowerCase() !== 'title')
         .map((option) => ({
           ...option,
           values: option.values.map((value) => {
-            const variants = product.variants.edges.filter((variant) =>
-              variant.node.selectedOptions.every((optionInVariant) => {
+            const variants = product.variants.edges.filter((variant) => {
+              return variant.node.selectedOptions.every((optionInVariant) => {
                 if (optionInVariant.name === option.name) {
                   return optionInVariant.value === value
-                }
-
-                if (selectedOptions[optionInVariant.name]) {
+                } else if (selectedOptions[optionInVariant.name]) {
                   return (
                     optionInVariant.value ===
                     selectedOptions[optionInVariant.name]
                   )
+                } else {
+                  return true
                 }
-
-                return true
               })
-            )
+            })
 
             const isNotAvailable = variants.every((v) =>
-              v
-                ? !v.node.availableForSale ||
-                  (v.node.quantityAvailable ?? 0) <= 0
-                : true
+              v ? !v.node.availableForSale : true
             )
 
-            return { value, notAvailable: isNotAvailable }
+            return { value, disabled: isNotAvailable }
           })
-        }))
-        .filter((o) => !!o),
+        })),
     [product.options, product.variants.edges, selectedOptions]
   )
 
-  const handleAddToBag = useCallback(
-    async (options?: { quantity?: number; openCartOnSuccess?: boolean }) => {
-      if (!selectedVariant) return
+  const handleAddToCart = useCallback(
+    async (options?: {
+      quantity?: number
+      openCartOnSuccess?: boolean
+      onSuccess?: () => void
+      onError?: (error: unknown) => void
+      onSettle?: () => void
+    }) => {
+      if (!selectedVariant) {
+        options?.onError?.(new Error('No variant selected'))
+        return
+      }
 
-      setAddToBagState('loading')
-      await onAddLineItem({
-        merchandiseId: selectedVariant.node.id,
-        quantity: options?.quantity ?? 1
-      })
-      setAddToBagState('success')
-
-      if (options?.openCartOnSuccess) {
-        cartToggleState.handleOn()
+      try {
+        await onAddLineItem({
+          merchandiseId: selectedVariant.node.id,
+          quantity: options?.quantity ?? 1
+        })
+        if (options?.openCartOnSuccess) {
+          cartToggleState.handleOn()
+        }
+        options?.onSuccess?.()
+      } catch (error) {
+        options?.onError?.(error)
+      } finally {
+        options?.onSettle?.()
       }
     },
     [cartToggleState, onAddLineItem, selectedVariant]
   )
 
-  const state = useMemo(
-    () => ({
-      addToBagState,
-      selectedOptions,
-      hasOneSelected: Object.keys(selectedOptions).some(
-        (o) => !!selectedOptions[o]
-      ),
-      hasAllSelected: Object.keys(selectedOptions).every(
-        (o) => !!selectedOptions[o]
-      )
-    }),
-    [addToBagState, selectedOptions]
-  )
+  const { hasOneOptionSelected, hasAllOptionsSelected, hasNoOptions } =
+    useMemo(() => {
+      const selectedOptionsKeys = Object.keys(selectedOptions)
+      return {
+        hasOneOptionSelected: selectedOptionsKeys.some(
+          (o) => !!selectedOptions[o]
+        ),
+        hasAllOptionsSelected: selectedOptionsKeys.every(
+          (o) => !!selectedOptions[o]
+        ),
+        hasNoOptions: selectedOptionsKeys.length === 0
+      }
+    }, [selectedOptions])
+
+  const discount = useMemo(() => {
+    if (!selectedVariant || !selectedVariant.node.compareAtPriceV2) return null
+    const raw =
+      selectedVariant.node.priceV2.amount -
+      selectedVariant.node.compareAtPriceV2.amount
+    const percentage =
+      (raw / selectedVariant.node.compareAtPriceV2.amount) * 100
+    return { percentage, raw }
+  }, [selectedVariant])
+
+  const isProductSoldOut = useMemo(() => {
+    return !product.availableForSale
+  }, [product.availableForSale])
+
+  const isSelectedVariantSoldOut = useMemo(() => {
+    return selectedVariant?.node.availableForSale
+  }, [selectedVariant])
 
   return {
     handleSelectOption,
-    handleAddToBag,
-    cleanOptions,
+    handleAddToCart,
+    optionsToSelect,
     selectedVariant,
-    state
+    discount,
+    isProductSoldOut,
+    isSelectedVariantSoldOut,
+    selectedOptions,
+    hasOneOptionSelected,
+    hasAllOptionsSelected,
+    hasNoOptions
   }
 }
